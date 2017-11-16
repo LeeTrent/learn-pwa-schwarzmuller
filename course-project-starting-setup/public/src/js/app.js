201962d675305308f379338cbb685553839a6a2d
LeeTrent <Lee.Trent.1@gmail.com>
2017-11-16 07:44:53 -0600

    Adds serviceWorker activation logic

--- course-project-starting-setup/public/src/js/app.js
+++ course-project-starting-setup/public/src/js/app.js
@@ -1,11 +1,11 @@
 if ( 'serviceWorker' in navigator) {
-    console.log('serviceWorker supported ...');
-    console.log('Registering serviceWorker ...');
+    console.log('[app.js] serviceWorker supported ...');
+    console.log('[app.js] Registering serviceWorker ...');
     navigator.serviceWorker
         .register('/sw.js')
         .then(function() {
-            console.log('serviceWorker registration has completed.');
+            console.log('[app.js] serviceWorker registration has completed.');
         });
 } else {
-    console.log('serviceWorker NOT SUPPORTED');
+    console.log('[app.js] serviceWorker NOT SUPPORTED');
 }
--- course-project-starting-setup/public/sw.js
+++ course-project-starting-setup/public/sw.js
@@ -1,3 +1,8 @@
 self.addEventListener('install', function(event) {
-    console.log('[serviceWorker] Installing serviceWorker ...', event);
+    console.log('[sw.js] Installing serviceWorker ...', event);
+});
+
+self.addEventListener('activate', function(event) {
+    console.log('[sw.js] Activating serviceWorker ...', event);
+    return self.clients.claim();
 });
