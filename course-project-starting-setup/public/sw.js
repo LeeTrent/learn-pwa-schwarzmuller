a64d0fb0590c9c36accf583568052bbb9b9d0d34
LeeTrent <Lee.Trent.1@gmail.com>
2017-11-16 19:27:14 -0600

    Adds fetch event listener handler

--- course-project-starting-setup/public/sw.js
+++ course-project-starting-setup/public/sw.js
@@ -6,3 +6,8 @@ self.addEventListener('activate', function(event) {
     console.log('[sw.js] Activating serviceWorker ...', event);
     return self.clients.claim();
 });
+
+self.addEventListener('fetch', function(event) {
+    console.log('[sw.js] Fetching ...', event);
+    event.respondWith(fetch(event.request));
+});
