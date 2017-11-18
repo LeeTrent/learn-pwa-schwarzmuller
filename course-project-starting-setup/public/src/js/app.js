792f5911c8259a85d7f55e83d76085a5aedad86b
LeeTrent <Lee.Trent.1@gmail.com>
2017-11-18 10:25:35 -0600

    Adds fetch API example in app.js

--- course-project-starting-setup/public/src/js/app.js
+++ course-project-starting-setup/public/src/js/app.js
@@ -16,3 +16,27 @@ window.addEventListener('beforeinstallprompt', function(event) {
     deferredPromptEvent = event;
     return false;
 });
+
+//////////////////////////////////////////////////
+// BEGIN: Fetch Example
+//////////////////////////////////////////////////
+fetch('http://httpbin.org/ip')
+.then(function(response) {
+    console.log('BEGIN: [app.js/fetch/response]:'); 
+    console.log(response); 
+    console.log(':END: [app.js/fetch/response]'); 
+    return response.json();
+})
+.then(function(data) {
+    console.log('BEGIN: [app.js/fetch/data]:'); 
+    console.log(data);
+    console.log(':END: [app.js/fetch/data]'); 
+})
+.catch(function(error) {
+    console.log('BEGIN: [app.js/fetch/error]:'); 
+    console.log(error);
+    console.log(':END: [app.js/fetch/error]'); 
+});
+//////////////////////////////////////////////////
+// END: Fetch Example
+//////////////////////////////////////////////////
