a81e232db26a763369907b419e525914a7b10a86
LeeTrent <Lee.Trent.1@gmail.com>
2017-11-18 19:13:43 -0600

    Adds fetch using 'POST' API example in app.js

--- course-project-starting-setup/public/src/js/app.js
+++ course-project-starting-setup/public/src/js/app.js
@@ -18,25 +18,60 @@ window.addEventListener('beforeinstallprompt', function(event) {
 });
 
 //////////////////////////////////////////////////
-// BEGIN: Fetch Example
+// BEGIN: Fetch GET Example
 //////////////////////////////////////////////////
 fetch('http://httpbin.org/ip')
 .then(function(response) {
-    console.log('BEGIN: [app.js/fetch/response]:'); 
+    console.log('BEGIN: [app.js/fetch/get/response]:'); 
     console.log(response); 
-    console.log(':END: [app.js/fetch/response]'); 
+    console.log(':END: [app.js/fetch/get/response]'); 
     return response.json();
 })
 .then(function(data) {
-    console.log('BEGIN: [app.js/fetch/data]:'); 
+    console.log('BEGIN: [app.js/fetch/get/data]:'); 
     console.log(data);
-    console.log(':END: [app.js/fetch/data]'); 
+    console.log(':END: [app.js/fetch/get/data]'); 
 })
 .catch(function(error) {
-    console.log('BEGIN: [app.js/fetch/error]:'); 
+    console.log('BEGIN: [app.js/fetch/get/error]:'); 
     console.log(error);
-    console.log(':END: [app.js/fetch/error]'); 
+    console.log(':END: [app.js/fetch/get/error]'); 
 });
 //////////////////////////////////////////////////
-// END: Fetch Example
+// END: Fetch GET Example
 //////////////////////////////////////////////////
+
+//////////////////////////////////////////////////
+// BEGIN: Fetch POST Example
+//////////////////////////////////////////////////
+fetch('http://httpbin.org/post', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    body: JSON.stringify (
+        { message: 'Does this work?' }
+    )
+})
+.then(function(response) {
+    console.log('BEGIN: [app.js/fetch/post/response]:'); 
+    console.log(response); 
+    console.log(':END: [app.js/fetch/post/response]'); 
+    return response.json();
+})
+.then(function(data) {
+    console.log('BEGIN: [app.js/fetch/post/data]:'); 
+    console.log(data);
+    console.log(':END: [app.js/fetch/post/data]'); 
+})
+.catch(function(error) {
+    console.log('BEGIN: [app.js/fetchpost/post/error]:'); 
+    console.log(error);
+    console.log(':END: [app.js/fetch/post/error]'); 
+});
+//////////////////////////////////////////////////
+// END: Fetch POST Example
+//////////////////////////////////////////////////
+
+
