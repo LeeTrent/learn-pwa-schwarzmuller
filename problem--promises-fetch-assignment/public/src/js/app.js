c34187d2caa77ddf9c65b4bd688d57f32b8d89c5
LeeTrent <Lee.Trent.1@gmail.com>
2017-11-20 19:42:07 -0600

    Adds 'PUT' request in 'start-button' click event.

--- problem--promises-fetch-assignment/public/src/js/app.js
+++ problem--promises-fetch-assignment/public/src/js/app.js
@@ -1,28 +1,61 @@
 
 var button = document.querySelector('#start-button');
 var output = document.querySelector('#output');
+var output2 = document.querySelector('#output2');
 
-button.addEventListener('click', function() {
-  // Create a new Promise here and use setTimeout inside the function you pass to the constructor
-  var promise = new Promise( function(resolve, reject) {
-    console.log("Timeout set ...");
-    setTimeout( function() {
-      console.log("Resolving url ...");
-      resolve('https://swapi.co/api/people/1');
-    }, 3000);
-  })
-  .then( function(url) {
-    console.log("Fetching url ...");
-    return fetch(url); // GET Request is the default - no config required
-  })
-  .then(function(response) {
-    console.log("Processing response ...");
-    return response.json();
-  })
-  .then(function(data) {
-    console.log("Assigning processed data to to 'output' div tag ...");
-    output.textContent = data.name;
-  });
+// button.addEventListener('click', function() {
+//   // Create a new Promise here and use setTimeout inside the function you pass to the constructor
+//   var promise = new Promise( function(resolve, reject) {
+//     console.log("Timeout set ...");
+//     setTimeout( function() {
+//       console.log("Resolving url ...");
+//       resolve('https://swapi.co/api/people/1');
+//     }, 3000);
+//   })
+//   .then( function(url) {
+//     console.log("Fetching url ...");
+//     return fetch(url); // GET Request is the default - no config required
+//   })
+//   .then(function(response) {
+//     console.log("Processing response ...");
+//     return response.json();
+//   })
+//   .then(function(data) {
+//     console.log("Assigning processed data to to 'output' div tag ...");
+//     output.textContent = data.name;
+//   });
+
+  button.addEventListener('click', function() {
+    // Create a new Promise here and use setTimeout inside the function you pass to the constructor
+    var promise = new Promise( function(resolve, reject) {
+      console.log("Timeout set ...");
+      setTimeout( function() {
+        console.log("Resolving PUT url ...");
+        resolve('https://httpbin.org/put');
+      }, 3000);
+    })
+    .then( function(url) {
+      console.log("Fetching PUT url ...");
+      return fetch(url, 
+        {
+          method: 'PUT',
+          headers: {
+            'Content-Type' : 'application/json',
+            'Accept' : 'application/json'
+          },
+          body: JSON.stringify(
+            { person: { name: 'Casey', age: 10 } }
+          )
+        }); // GET Request is the default - no config required
+    })
+    .then(function(response) {
+      console.log("Processing PUT response ...");
+      return response.json();
+    })
+    .then(function(data) {
+      console.log("Assigning processed data to to 'output' div tag ...");
+      output2.textContent = data.json.person.name + ' / ' + data.json.person.age;
+    });
 
   // Handle the Promise "response" (=> the value you resolved) and return a fetch()
   // call to the value (= URL) you resolved (use a GET request)
