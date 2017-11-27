2068c3655320ebe24b25883177891b148001e2c0
LeeTrent <Lee.Trent.1@gmail.com>
2017-11-27 07:22:44 -0600

    Prepends 'return' to caches.open in the server worker fetch event in sw-caching-02--after-cleanup project

--- sw-caching-02--after-cleanup/public/sw.js
+++ sw-caching-02--after-cleanup/public/sw.js
@@ -53,7 +53,7 @@ self.addEventListener('fetch', function(event) {
         } else {
           return fetch(event.request)
             .then(function(res) {
-              caches.open(CACHE_VERSION_DYNAMIC)
+              return caches.open(CACHE_VERSION_DYNAMIC)
                 .then(function(cache) {
                   cache.put(event.request.url, res.clone());
                   return res;
